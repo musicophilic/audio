492a9c5dc44630a8d171da31da797e4c83701e0c
Mayur Mahrotri <mayur@mayurs-macbook-pro.local>
2016-02-06 14:58:33 +0530

    Initial Commit

---
+++ djwalebabu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  djwalebabu
+//
+//  Created by Mayur Mahrotri on 06/02/16.
+//  Copyright © 2016 Mayur Mahrotri. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ djwalebabu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  djwalebabu
+//
+//  Created by Mayur Mahrotri on 06/02/16.
+//  Copyright © 2016 Mayur Mahrotri. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
